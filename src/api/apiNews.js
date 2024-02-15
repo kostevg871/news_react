52d01a0ba1432c48c759272a3194c9127e8bc29e
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-02-15 21:50:36 +0300

    add pagination main, change api endpoint

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
 const API_URL = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async () => {
+export const getNews = async (page_number = 1, page_size = 10) => {
   try {
-    const response = await axios.get(`${BASE_URL}latest-news`, {
-      params: { apiKey: API_URL },
+    const response = await axios.get(`${BASE_URL}search`, {
+      params: { apiKey: API_URL, page_number, page_size },
     });
     console.log(response.data);
     return response.data;
